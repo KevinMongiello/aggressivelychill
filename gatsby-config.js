62f0c77c85e2e77997380b92314fecfd4fc30941
Kevin Mongiello <kmong92@gmail.com>
2018-10-15 00:30:34 -0400

    Add pineapple favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'AgressivelyChill',
+        short_name: 'AgressivelyChill',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.jpg', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
